ea6d6f739c8494ad0888d14808c8a21c9c6413a5
Manuela Gomes <manudeveloper82@gmail.com>
2023-03-13 18:55:14 -0300

    css messages

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {faTimes} from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent {
+export class MessagesComponent implements OnInit {
+  faTimes = faTimes
+
+  ngOnInit(): void {}
 
 }
